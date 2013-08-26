54a490049a85075051160ebc4dd02550fcaa2def
Jim Karg <jkarg@intertech.com>
2013-08-26 16:10:43 -0500

    initial method stub out, basic FTP functions

---
+++ Ftp.TestClient/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ftp.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DownloadFile();
+            Console.ReadLine();
+
+            UploadFile();
+            Console.ReadLine();
+
+            ListContents();
+            Console.ReadLine();
+        }
+
+        private static void ListContents()
+        {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://localhost/");
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("anonymous", "a@b.com");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.WriteLine("Directory List Complete, status {0}", response.StatusDescription);
+
+            reader.Close();
+            response.Close();
+        }
+
+        private static void UploadFile()
+        {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://localhost/test.upload.txt");
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential ("anonymous","a@b.c");
+            
+            // Copy the contents of the file to the request stream.
+            StreamReader sourceStream = new StreamReader(@"C:\ftp\test.txt");
+            byte [] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+    
+            Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+    
+            response.Close();
+        }
+
+        private static void DownloadFile()
+        {
+            // Get the object used to communicate with the server.
+            var request = (FtpWebRequest)WebRequest.Create("ftp://localhost/test.txt");
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("anonymous", "a@b.c");
+
+            var response = (FtpWebResponse)request.GetResponse();
+
+            var responseStream = response.GetResponseStream();
+            var reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.WriteLine("Download Complete, status {0}", response.StatusDescription);
+
+            reader.Close();
+            response.Close();
+        }
+    }
+}
