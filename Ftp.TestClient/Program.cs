c6cbc4c8eb00b54f944b093f254431a1484b836d
Jim Karg <jkarg@intertech.com>
2013-08-26 21:42:18 -0500

    Now uploads the file it downloads

--- Ftp.TestClient/Program.cs
+++ Ftp.TestClient/Program.cs
@@ -7,14 +7,14 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using NLog;
- 
-
+using System.Configuration;
 
 namespace Ftp.TestClient
 {
     class Program
     {
         static Logger logger = LogManager.GetCurrentClassLogger();
+        static Random random = new Random();
 
         static void Main(string[] args)
         {
@@ -28,9 +28,14 @@ namespace Ftp.TestClient
         {
             try
             {
-                DownloadFile();
-                UploadFile();
-                //ListContents();
+                var file = DownloadFile("test.txt");
+
+                // Copy the contents of the file to the request stream.
+                var sourceStream = new StreamReader(file);
+                var fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+                sourceStream.Close();
+                file.Close();
+                UploadFile(random.Next().ToString(), fileContents);
             }
             catch (Exception e)
             {
@@ -38,42 +43,18 @@ namespace Ftp.TestClient
             }
         }
 
-        private static void ListContents()
+        private static void UploadFile(string fileName, byte [] fileContents)
         {
+            var serverAddress = ConfigurationManager.AppSettings["FtpServerAddress"];
+            var uploadDirectory = ConfigurationManager.AppSettings["UploadDirectory"];
+
             // Get the object used to communicate with the server.
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://localhost/");
-            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
-
-            // This example assumes the FTP site uses anonymous logon.
-            request.Credentials = new NetworkCredential("anonymous", "a@b.com");
-
-            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-
-            Stream responseStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(responseStream);
-            logger.Debug(reader.ReadToEnd());
-
-            logger.Info("Directory List Complete, status {0}", response.StatusDescription);
-            //Console.WriteLine("Directory List Complete, status {0}", response.StatusDescription);
-
-            reader.Close();
-            response.Close();
-        }
-
-        private static void UploadFile()
-        {
-            var random = new Random();
-            // Get the object used to communicate with the server.
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(String.Format("ftp://localhost/test.{0}.upload.txt", random.Next().ToString()));
+            var request = (FtpWebRequest)WebRequest.Create(String.Format("{0}{1}/{2}", serverAddress, uploadDirectory, fileName));
             request.Method = WebRequestMethods.Ftp.UploadFile;
 
             // This example assumes the FTP site uses anonymous logon.
             request.Credentials = new NetworkCredential ("anonymous","a@b.c");
             
-            // Copy the contents of the file to the request stream.
-            StreamReader sourceStream = new StreamReader(@"C:\ftp\test.txt");
-            byte [] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
-            sourceStream.Close();
             request.ContentLength = fileContents.Length;
 
             Stream requestStream = request.GetRequestStream();
@@ -83,14 +64,16 @@ namespace Ftp.TestClient
             FtpWebResponse response = (FtpWebResponse)request.GetResponse();
     
             logger.Info("Upload File Complete, status {0}", response.StatusDescription);
-    
             response.Close();
         }
 
-        private static void DownloadFile()
+        private static Stream DownloadFile(string fileName)
         {
+            var serverAddress = ConfigurationManager.AppSettings["FtpServerAddress"];
+            var uploadDirectory = ConfigurationManager.AppSettings["DownloadDirectory"];
+
             // Get the object used to communicate with the server.
-            var request = (FtpWebRequest)WebRequest.Create("ftp://localhost/test.txt");
+            var request = (FtpWebRequest)WebRequest.Create(String.Format("{0}{1}/{2}", serverAddress, uploadDirectory, fileName));
             request.Method = WebRequestMethods.Ftp.DownloadFile;
 
             // This example assumes the FTP site uses anonymous logon.
@@ -99,13 +82,15 @@ namespace Ftp.TestClient
             var response = (FtpWebResponse)request.GetResponse();
 
             var responseStream = response.GetResponseStream();
-            var reader = new StreamReader(responseStream);
-            Console.WriteLine(reader.ReadToEnd());
+            var memoryStream = new MemoryStream();
+            responseStream.CopyTo(memoryStream);
+
 
             logger.Info("Download Complete, status {0}", response.StatusDescription);
 
-            reader.Close();
             response.Close();
+
+            return memoryStream;
         }
     }
 }
